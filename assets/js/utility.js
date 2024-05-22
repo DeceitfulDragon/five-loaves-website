a670f3e1870e0dd301e87af21779880a63a2bd27
CatGettingHitInTheFace <feartherenegade@gmail.com>
2024-05-21 19:13:16 -0500

    Moved pages into /pages/ and attempted to add a service worker

--- assets/js/utility.js
+++ assets/js/utility.js
@@ -1,5 +1,23 @@
-// Grabs config.js values so that things can be changed easily, future-proofing
-import { config } from './config.js';
+const registerServiceWorker = async () => {
+    if ("serviceWorker" in navigator) {
+      try {
+        const registration = await navigator.serviceWorker.register("/service-worker.js", {
+          scope: "/",
+        });
+        if (registration.installing) {
+          console.log("Service worker installing");
+        } else if (registration.waiting) {
+          console.log("Service worker installed");
+        } else if (registration.active) {
+          console.log("Service worker active");
+        }
+      } catch (error) {
+        console.error(`Registration failed with ${error}`);
+      }
+    }
+  };
+
+registerServiceWorker();
 
 adjustContentMargin();
 $(window).resize(adjustContentMargin);
@@ -10,6 +28,60 @@ function adjustContentMargin() {
     var navbarHeight = $('.navbar-custom').outerHeight(); // Get the outer height of the navbar
     $('main').css('padding-top', navbarHeight); // main padding
 }
+
+// == START CONFIG ===========================================================================================================================================
+const config = {
+    donateLink: '',
+
+    foodShelfNumber: '8675-309',
+    thriftStoreNumber: '8675-309',
+
+    foodShelfEmail: 'contact@email.com',
+    thriftStoreEmail: 'contact@email.com',
+
+    foodShelfFacebook: '',
+    thriftStoreFacebook: '',
+
+    fiveLoavesBlog: '',
+
+    csfpFormLink: '',
+    volunteerFormLink: '',
+
+    poBox: `Some Place
+    P.O. Box 111
+    Some Location, MN`,
+
+    foodShelfLocation: '222 East 5th Street, Suite A',
+    thriftStoreLocation: '222 South 5th Road',
+
+    cityAndState: 'Some Location, MN',
+
+    comeMealLocation: 'Some Location',
+
+    comeMonths: {
+        January: "Some Location",
+        February: "Some Location",
+        March: "Some Location",
+        April: "Some Location",
+        May: "Some Location",
+        June: "Some Location",
+        July: "Some Location",
+        August: "Some Location",
+        September: "Some Location",
+        October: "Some Location",
+        November: "Some Location",
+        December: "Some Location"
+    },
+
+    districtOffice: 'Some Location',
+    districtOfficeMail: 'Some Location',
+
+    foodShelfMapLink: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11645.407732417327!2d-88.00744937548164!3d43.139138545724975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88051d373f526447%3A0x21ef9b0ae9dc212d!2sMilwaukee-Northwest%20DMV!5e0!3m2!1sen!2sus!4v1713949960674!5m2!1sen!2sus',
+    thriftStoreMapLink: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11645.407732417327!2d-88.00744937548164!3d43.139138545724975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88051d373f526447%3A0x21ef9b0ae9dc212d!2sMilwaukee-Northwest%20DMV!5e0!3m2!1sen!2sus!4v1713949960674!5m2!1sen!2sus',
+};
+
+
+// == END CONFIG ===========================================================================================================================================
 
 // For the Food Shelf Phone number
 $('#FoodShelfPhone').each(function () {
@@ -66,9 +138,6 @@ $('#districtOffice').text(config.districtOffice);
 $('#districtOfficeMail').text(config.districtOfficeMail);
 $('#comeMealLocation').text(config.comeMealLocation);
 
-// Adds months with their respective locations in the Come Meal organization
-populateCommunityList()
-
 function populateCommunityList() {
     const ul = $(`.comeMealList`);
     Object.entries(config.comeMonths).forEach(([month, community]) => {
@@ -77,6 +146,9 @@ function populateCommunityList() {
         ul.append(li);
     });
 }
+
+// Adds months with their respective locations in the Come Meal organization
+populateCommunityList()
 
 // Rotates the arrow on the organization cards and opens the dropdown
 $('.org-row').on('click', function () {
