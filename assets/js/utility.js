3ba33dcf0147a5fcfe189be435638de585ca01e9
CatGettingHitInTheFace <feartherenegade@gmail.com>
2024-06-26 07:30:54 -0500

    Removed the padding adjuster because I found a better way

--- assets/js/utility.js
+++ assets/js/utility.js
@@ -1,12 +1,3 @@
-// For changing the padding based on the height of the navbar
-// Did it this way so people can scale the web page without the navbar overlapping things
-function adjustContentMargin() {
-    var navbarHeight = $('.navbar-custom').outerHeight(); // Get the outer height of the navbar
-    $('main').css('padding-top', navbarHeight); // main padding
-}
-adjustContentMargin();
-$(window).resize(adjustContentMargin);
-
 // == START CONFIG ===========================================================================================================================================
 const config = {
     donateLink: '',
@@ -45,8 +36,6 @@ const config = {
     foodShelfMapLink: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11645.407732417327!2d-88.00744937548164!3d43.139138545724975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88051d373f526447%3A0x21ef9b0ae9dc212d!2sMilwaukee-Northwest%20DMV!5e0!3m2!1sen!2sus!4v1713949960674!5m2!1sen!2sus',
     thriftStoreMapLink: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11645.407732417327!2d-88.00744937548164!3d43.139138545724975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88051d373f526447%3A0x21ef9b0ae9dc212d!2sMilwaukee-Northwest%20DMV!5e0!3m2!1sen!2sus!4v1713949960674!5m2!1sen!2sus',
 };
-
-
 // == END CONFIG ===========================================================================================================================================
 
 // For the Food Shelf Phone number
